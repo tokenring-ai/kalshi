040449bafdd7d9c573e478905b7dee9f48f38841
Mark Dierolf <mdierolf@cloudquote.io>
2026-04-16 08:41:55 -0700

    Simplify tools structure and update `addTools` usage in Kalshi plugin.

--- tools.ts
+++ tools.ts
@@ -3,9 +3,9 @@ import getMarkets from "./tools/getMarkets.ts";
 import getOrderbook from "./tools/getOrderbook.ts";
 import getSeries from "./tools/getSeries.ts";
 
-export default {
+export default [
   getSeries,
   getMarkets,
   getEvent,
   getOrderbook,
-};
+];
